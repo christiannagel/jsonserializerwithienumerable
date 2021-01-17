ad3686abd46db1c12e8d4e14e18821821a2078fc
Christian Nagel <christian@christiannagel.com>
2021-01-17 18:50:19 +0100

    records implement IList for deserialization

--- JsonSerializerIssue/JsonSerializerIssue/Program.cs
+++ JsonSerializerIssue/JsonSerializerIssue/Program.cs
@@ -7,33 +7,43 @@ using System.Text.Json;
 //{
 //    new Category("Appetizers")
 //    {
-//        new Item("Dungeon Crab Cocktail", "Classic coctail sauce", 27M),
-//        new Item("Almond Custed Scallops", "Almonds, Parmesan, chive buerre blanc", 19M)
+//        new Item("Dungeon Crab Cocktail", "Classic cocktail sauce", 27M),
+//        new Item("Almond Crusted Scallops", "Almonds, Parmesan, chive beurre blanc", 19M)
 //    },
 //    new Category("Dinner")
 //    {
-//        new Item("Grilled King Salmon", "Lemon chive buerre blanc", 49M)
+//        new Item("Grilled King Salmon", "Lemon chive beurre blanc", 49M)
 //    }
 //};
 
 Category appetizers = new("Appetizers");
-appetizers.Items.Add(new Item("Dungeon Crab Cocktail", "Classic coctail sauce", 27M));
+appetizers.Items.Add(new Item("Dungeon Crab Cocktail", "Classic cocktail sauce", 27M));
 appetizers.Items.Add(new Item("Almond Crusted Scallops", "Almonds, Parmesan, chive beurre blanc", 19M));
 
 Category dinner = new("Dinner");
-dinner.Items.Add(new Item("Grilled King Salmon", "Lemon chive buerre blanc", 49M));
+dinner.Items.Add(new Item("Grilled King Salmon", "Lemon chive beurre blanc", 49M));
 
 Card card = new("The Restaurant");
 card.Categories.Add(appetizers);
 card.Categories.Add(dinner);
 
 string json = SerializeJson(card);
+// string json = File.ReadAllText("thecard.json");
 DeserializeJson(json);
 
 
 string SerializeJson(Card card)
 {
     Console.WriteLine(nameof(SerializeJson));
+
+    //JsonSerializerOptions options = new()
+    //{
+    //    WriteIndented = true,
+    //    //Converters =
+    //    //{
+    //    //    new System.Text.Json.Serialization.JsonConverter<object>
+    //    //}
+    //};
 
     string json = JsonSerializer.Serialize(card);
     Console.WriteLine(json);
@@ -82,30 +92,76 @@ static void DeserializeJson(string json)
    at <Program>$.<Main>$(String[] args) in C:\github\jsonserializerwithienumerable\JsonSerializerIssue\JsonSerializerIssue\Program.cs:line 31
  * */
 public record Item(string Title, string Text, decimal Price);
-public record Category(string Title) : IEnumerable<Item>
+public record Category(string Title) : IList<Item>
 {
+    public Category() : this(string.Empty)
+    {
+    }
     public IList<Item> Items { get; init; } = new List<Item>();
+
+    public int Count => Items.Count;
+
+    public bool IsReadOnly => false;
+
+    public Item this[int index] { get => Items[index]; set => Items[index] = value; }
 
     public IEnumerator<Item> GetEnumerator() => Items.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)Items).GetEnumerator();
 
     public void Add(Item item) => Items.Add(item);
+
+    public int IndexOf(Item item) => Items.IndexOf(item);
+
+    public void Insert(int index, Item item) => Items.Insert(index, item);
+
+    public void RemoveAt(int index) => Items.RemoveAt(index);
+
+    public void Clear() => Items.Clear();
+
+    public bool Contains(Item item) => Items.Contains(item);
+
+    public void CopyTo(Item[] array, int arrayIndex) => Items.CopyTo(array, arrayIndex);
+
+    public bool Remove(Item item) => Items.Remove(item);
 }
-public record Card(string Title) : IEnumerable<Category>
+public record Card(string Title) : IList<Category>
 {
+    public Card() : this(string.Empty) { }
+
     public IList<Category> Categories { get; init; } = new List<Category>();
+
+    public int Count => Categories.Count;
+
+    public bool IsReadOnly => false;
+
+    public Category this[int index] { get => Categories[index]; set => Categories[index] = value; }
 
     public IEnumerator<Category> GetEnumerator() => Categories.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)Categories).GetEnumerator();
 
-    public void Add(Category category) => Categories.Add(category);
+    public void Add(Category item) => Categories.Add(item);
+
+    public int IndexOf(Category item) => Categories.IndexOf(item);
+
+    public void Insert(int index, Category item) => Categories.Insert(index, item);
+
+    public void RemoveAt(int index) => Categories.RemoveAt(index);
+
+    public void Clear() => Categories.Clear();
+
+    public bool Contains(Category item) => Categories.Contains(item);
+
+    public void CopyTo(Category[] array, int arrayIndex) => Categories.CopyTo(array, arrayIndex);
+
+    public bool Remove(Category item) => Categories.Remove(item);
+
 }
 
 //// this deserializes successfully:
 //public record Item(string Title, string Text, decimal Price);
-//public record Category(string Title) 
+//public record Category(string Title)
 //{
 //    public IList<Item> Items { get; init; } = new List<Item>(); // init required for deserialization
 
@@ -115,7 +171,7 @@ public record Card(string Title) : IEnumerable<Category>
 
 //    public void Add(Item item) => Items.Add(item);
 //}
-//public record Card(string Title) 
+//public record Card(string Title)
 //{
 //    public IList<Category> Categories { get; init; } = new List<Category>();
 
